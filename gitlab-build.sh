6e611632b24bfa1c5482fbbb0c3553a77025209e
Deepak Khatri <lorforlinux@beagleboard.org>
2022-09-01 18:45:50 +0530

    Update CI and build script

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -28,13 +28,13 @@ sphinx-build -M latexpdf . public/$CI_COMMIT_BRANCH/
 mv public/$CI_COMMIT_BRANCH/latex/beagleboard-docs.pdf public/$CI_COMMIT_BRANCH/
 rm -rf public/$CI_COMMIT_BRANCH/latex
 
-elif [ "$CI_COMMIT_TAG" != "" ]; then
+# elif [ "$CI_COMMIT_TAG" != "" ]; then
 
-export GIT_BRANCH=$(git branch -a --contains tags/$CI_COMMIT_TAG | grep origin | sed 's/.*origin\///')
-sphinx-build -b html . public/$GIT_BRANCH/
-sphinx-build -M latexpdf . public/$GIT_BRANCH/
-mv public/$GIT_BRANCH/latex/beagleboard-docs.pdf public/$GIT_BRANCH/beagleboard-docs-$CI_COMMIT_TAG.pdf
-ln -s public/$GIT_BRANCH/latex/beagleboard-docs-$CI_COMMIT_TAG.pdf public/$GIT_BRANCH/beagleboard-docs.pdf
-rm -rf public/$GIT_BRANCH/latex
+# export GIT_BRANCH=$(git branch -a --contains tags/$CI_COMMIT_TAG | grep origin | sed 's/.*origin\///')
+# sphinx-build -b html . public/$GIT_BRANCH/
+# sphinx-build -M latexpdf . public/$GIT_BRANCH/
+# mv public/$GIT_BRANCH/latex/beagleboard-docs.pdf public/$GIT_BRANCH/beagleboard-docs-$CI_COMMIT_TAG.pdf
+# ln -s public/$GIT_BRANCH/latex/beagleboard-docs-$CI_COMMIT_TAG.pdf public/$GIT_BRANCH/beagleboard-docs.pdf
+# rm -rf public/$GIT_BRANCH/latex
 
 fi
