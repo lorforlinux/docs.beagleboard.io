b9e0da3d94535528e0fc85e96926190b20d685ec
Jason Kridner <jkridner@beagleboard.org>
2024-06-04 11:44:21 -0400

    missed a cp

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -76,12 +76,12 @@ HERE
 
 		# Update docs.beagleboard.org
 		if [ "$CI_COMMIT_TAG" != "" ]; then
-			if [ "$VER_DIR" = "latest" ]; then
-				cp public/index.html /var/www/docs
-			fi
 			mkdir -p ~/.ssh
 			eval "$(ssh-agnet -s)"
 			echo "${PRIVATE_KEY}" | base64 -d | ssh-add -
+			if [ "$VER_DIR" = "latest" ]; then
+				rsync -v -a --delete public/index.html docs@beagleboard.org:/var/www/docs/
+			fi
 			rsync -v -a --delete public/$VER_DIR/. docs@beagleboard.org:/var/www/docs/$VER_DIR
 		fi
 	fi
